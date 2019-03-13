159bf43927bea1295569c591c801166fa578efec
jing.wang <jing.wang@fengjr.com>
2019-03-13 16:49:21 +0800

    ee
    ee

---
+++ precache-manifest.937fb09197b44afa8909abb2bc1e0366.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = [
+  {
+    "revision": "98c5990c7cb1a5e9c263",
+    "url": "./static/css/main.b63b0c3c.chunk.css"
+  },
+  {
+    "revision": "98c5990c7cb1a5e9c263",
+    "url": "./static/js/main.398ebfa5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "646e465feb9264250d0d",
+    "url": "./static/js/2.bd3c64a4.chunk.js"
+  },
+  {
+    "revision": "522478ce890aa1ac4da2811bec02914b",
+    "url": "./static/media/almendra-bold-webfont.522478ce.woff"
+  },
+  {
+    "revision": "9da85be3ed0af73f3c6a475fbea36c96",
+    "url": "./static/media/almendra-bold-webfont.9da85be3.woff2"
+  },
+  {
+    "revision": "14eb79a4542eb017408319befb168fbf",
+    "url": "./static/media/almendra-regular-webfont.14eb79a4.woff"
+  },
+  {
+    "revision": "96c4be2dc6874fb4f94a0b92ae49b85e",
+    "url": "./static/media/Game_Profile_background.96c4be2d.jpg"
+  },
+  {
+    "revision": "1a1efde8dde8093b3273466c6e814df3",
+    "url": "./static/media/Game_Login_background.1a1efde8.jpg"
+  },
+  {
+    "revision": "09afde59b0dc29f290b3fa2ee14cd1bc",
+    "url": "./static/media/Game_background.09afde59.jpg"
+  },
+  {
+    "revision": "8586386fa7c16532439def5196381940",
+    "url": "./static/media/Game_buttons1.8586386f.png"
+  },
+  {
+    "revision": "1189335ab52236af404c0c0bd2a2a3d0",
+    "url": "./static/media/Game_buttons3.1189335a.png"
+  },
+  {
+    "revision": "f63e346f51771c36f6b3eb6bd47b7561",
+    "url": "./static/media/Game_buttons2.f63e346f.png"
+  },
+  {
+    "revision": "82ede40d00ec982bcd47e17a68b24324",
+    "url": "./static/media/logo_loading.82ede40d.png"
+  },
+  {
+    "revision": "45ba1dde98ad9a4f27b459adf657e303",
+    "url": "./static/media/c1.45ba1dde.png"
+  },
+  {
+    "revision": "a76230c32d0e7bb33adecf75134d7090",
+    "url": "./static/media/c6.a76230c3.png"
+  },
+  {
+    "revision": "34aa7774d1d5a578c66693c18b9592e6",
+    "url": "./static/media/c11.34aa7774.png"
+  },
+  {
+    "revision": "78a6ee41919927f1b9a20bffcd0adb0b",
+    "url": "./static/media/c3.78a6ee41.png"
+  },
+  {
+    "revision": "14f419ffb4589ec9289c328a566c07ec",
+    "url": "./static/media/c8.14f419ff.png"
+  },
+  {
+    "revision": "e1df64762f35489a43fdffc5435feee1",
+    "url": "./static/media/c15.e1df6476.png"
+  },
+  {
+    "revision": "b7361c64be06c1d8d789fb4cda5b6300",
+    "url": "./static/media/c16.b7361c64.png"
+  },
+  {
+    "revision": "77a3c4e4637ce8e602e72ee8532a0daf",
+    "url": "./static/media/c5.77a3c4e4.png"
+  },
+  {
+    "revision": "ce5ccdb16fcf5bce0694cdd4260ecdb2",
+    "url": "./static/media/c10.ce5ccdb1.png"
+  },
+  {
+    "revision": "2092b0397b4fadbc90291fa2cf0f5757",
+    "url": "./static/media/c13.2092b039.png"
+  },
+  {
+    "revision": "d5f356634479e971ae2a28086c03d1c9",
+    "url": "./static/media/c17.d5f35663.png"
+  },
+  {
+    "revision": "2a4c5ecc0e898d106447f79084747355",
+    "url": "./static/media/Side_component_label.2a4c5ecc.png"
+  },
+  {
+    "revision": "355790f61ec28047045c50badff042d7",
+    "url": "./static/media/Side_component_diagram.355790f6.png"
+  },
+  {
+    "revision": "25506ee299d492d46a1ac7881b7304c0",
+    "url": "./static/media/Side_component_frame.25506ee2.png"
+  },
+  {
+    "revision": "6e334a6d0949c9a855572ed6b6ae6f87",
+    "url": "./static/media/title_rules.6e334a6d.png"
+  },
+  {
+    "revision": "0dfbac78f6bf35be1f659f3a6fccfc3b",
+    "url": "./static/media/card_type.0dfbac78.png"
+  },
+  {
+    "revision": "5c1cf761169d1a14ad094d5c4205be36",
+    "url": "./static/media/special_card_0.5c1cf761.png"
+  },
+  {
+    "revision": "92f475066cfd83ca4f104060bf132277",
+    "url": "./static/media/attack_power.92f47506.png"
+  },
+  {
+    "revision": "e1cddb59cbf4f4b6f6d700a73a1af188",
+    "url": "./static/media/special_card_3.e1cddb59.png"
+  },
+  {
+    "revision": "197be005ec6f47b787dc2f611571c6ae",
+    "url": "./static/media/old_paper_bg.197be005.png"
+  },
+  {
+    "revision": "f912e8dae3c3fea43d8da1e0ad72a24e",
+    "url": "./static/media/bg_rules.f912e8da.png"
+  },
+  {
+    "revision": "135b0b2173e84661cfd540a0c161d42b",
+    "url": "./static/media/Player_1card.135b0b21.png"
+  },
+  {
+    "revision": "2e129bbae7d1542defd216fe50990acd",
+    "url": "./static/media/Player_4card.2e129bba.png"
+  },
+  {
+    "revision": "ccbf882c3c0e0f7dbdc95a3e5b574868",
+    "url": "./static/media/Player_3card.ccbf882c.png"
+  },
+  {
+    "revision": "87e2ed07db22943f35e0c31045bb0982",
+    "url": "./static/media/Player_2card.87e2ed07.png"
+  },
+  {
+    "revision": "e307d40ba24dc7830b77cb0d776f9293",
+    "url": "./static/media/CPU_1card.e307d40b.png"
+  },
+  {
+    "revision": "96370d4db024dbed5708a9d23b0820e8",
+    "url": "./static/media/CPU_3card.96370d4d.png"
+  },
+  {
+    "revision": "2933265b465f75e86c259dbcdb59139d",
+    "url": "./static/media/CPU_4card.2933265b.png"
+  },
+  {
+    "revision": "12571dac59a81804773c8ea1f05ed41a",
+    "url": "./static/media/CPU_2card.12571dac.png"
+  },
+  {
+    "revision": "851eebb9541ea4759cf455e008a1d933",
+    "url": "./static/media/Game_board_wCards.851eebb9.png"
+  },
+  {
+    "revision": "40bdd1f806c244180653674ae9bb8c1f",
+    "url": "./static/media/CPU_component_empty.40bdd1f8.png"
+  },
+  {
+    "revision": "69c31f11ef7496c10c83ecd57c2c805f",
+    "url": "./static/media/CPU_component_full.69c31f11.png"
+  },
+  {
+    "revision": "bb6e7b0f61fd0fde984850b03ddd057a",
+    "url": "./static/media/Player_component_full.bb6e7b0f.png"
+  },
+  {
+    "revision": "e4bea507f27bcf5de46c06e50dad81ac",
+    "url": "./static/media/Player_component_empty.e4bea507.png"
+  },
+  {
+    "revision": "2a6872b133db66970c76144373da3386",
+    "url": "./static/media/Player_component_frame.2a6872b1.png"
+  },
+  {
+    "revision": "537a294ed8962c880826227f2352d047",
+    "url": "./static/media/CPU_component_frame.537a294e.png"
+  },
+  {
+    "revision": "3eb46b0feb59ea872ce6aeb629092e6c",
+    "url": "./static/media/Game_Profile_icon.3eb46b0f.png"
+  },
+  {
+    "revision": "9b53ec811c5c33fd87f692443240b506",
+    "url": "./static/media/Game_Profile_smframe.9b53ec81.png"
+  },
+  {
+    "revision": "4b62f8bf1642e5ac894acd00a07618c3",
+    "url": "./static/media/Game_logo.4b62f8bf.png"
+  },
+  {
+    "revision": "8c78a9470ae257f55ff4a7389e9dfaf1",
+    "url": "./static/media/Game_Profile_frame.8c78a947.png"
+  },
+  {
+    "revision": "b7e02044a5b9a8b4c5b2444207fd0f78",
+    "url": "./static/media/name_bg_with_shadow.b7e02044.png"
+  },
+  {
+    "revision": "4587de2da2a3b7f942a260eb6eca33f7",
+    "url": "./static/media/title_defeat.4587de2d.png"
+  },
+  {
+    "revision": "d899294590c8f69fa654c4eb24c05ace",
+    "url": "./static/media/CPU_backforflip.d8992945.png"
+  },
+  {
+    "revision": "08d63520fb4aa9ac25c14652600ba263",
+    "url": "./static/media/title_victory.08d63520.png"
+  },
+  {
+    "revision": "f640ad59b841299dc9309ae98122edda",
+    "url": "./static/media/Player_backforflip.f640ad59.png"
+  },
+  {
+    "revision": "f997c80d2f853fa9a1eb3bc432b20c9e",
+    "url": "./static/media/popup_bg.f997c80d.png"
+  },
+  {
+    "revision": "7f5578801515c37f97af77fbd7a51230",
+    "url": "./static/media/Game_Login_form.7f557880.png"
+  },
+  {
+    "revision": "173a74c8c093af45a730625f976fa006",
+    "url": "./index.html"
+  }
+];
